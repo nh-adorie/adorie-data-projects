ec5786bab8951dc03352e3e87938edddf8f3a4da
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-10-09 03:43:31 +0000

    add folder scm-co-smart-project

---
+++ scm-co-smart-project/notebook/.ipynb_checkpoints/clean_data-checkpoint.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("/workspaces/scm-co-smart-project/data/raw/DataCoSupplyChainDataset.csv", encoding = 'latin-1' )
+print(df)
---
+++ scm-co-smart-project/scripts/download_data.py
@@ -0,0 +1,3 @@
+import kagglehub
+path = kagglehub.dataset_download("shashwatwork/dataco-smart-supply-chain-for-big-data-analysis")
+print("Path to dataset files:", path)
