7d51fa3331f3b9f2d7cfaab17d0912f99f06d4db
Adorie <113771185+nh-adorie@users.noreply.github.com>
2025-10-20 03:39:14 +0000

    new project

---
+++ customer-personality-analysis/scripts/download_data.py
@@ -0,0 +1,7 @@
+import kagglehub
+path = kagglehub.dataset_download("imakash3011/customer-personality-analysis")
+
+print(f"\nChạy lệnh này trong terminal để copy file:")
+print(f"cp {path}/* ./")
+
+# https://www.kaggle.com/code/karnikakapoor/customer-segmentation-clustering/notebook
